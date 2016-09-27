7955851c7b1e339e8f2594c3e09e800f28073d70
jpmazza <jp.mazza@toptierlabs.com>
2016-09-27 18:16:46 -0300

    Tabla para mostrar las ventas

--- src/containers/PresalePage.js
+++ src/containers/PresalePage.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+import '../styles/presales.scss';
 
 class PresalePage extends Component {
   constructor(props, context) {
     super(props, context);
   }
 
-  priceFormatter(cell, row){
+  priceFormatter(cell){
     return '<i class="glyphicon glyphicon-usd"></i> ' + cell;
+  }
+
+  dateFormatter(cell){
+    /*Esto parece que esta convirtiendo teniendo en cuenta
+    el timezone. Luego que se obtenga la fecha del backend
+    se va a testear este metodo para ver si funciona bien.*/
+    const date = new Date(cell);
+    const day = date.getDate();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+    return day + '/' + month + '/' + year;
   }
 
   render() {
     const sales = [{
       id: 1,
-      date: '2016-07-1',
+      date: '2016-07-01',
       seller: "Vendedor 1",
       productId: "123",
       product: "Azucar",
@@ -60,7 +72,7 @@ class PresalePage extends Component {
       <div className="table-wrapper">
         <BootstrapTable data={sales} striped={true} hover={true} search={true} pagination={true}>
           <TableHeaderColumn dataField="id" isKey={true} dataSort={true}>ID Venta</TableHeaderColumn>
-          <TableHeaderColumn dataField="date" sortFunc={dateSorter} dataSort={true}>Fecha</TableHeaderColumn>
+          <TableHeaderColumn dataField="date" sortFunc={dateSorter} dataSort={true} dataFormat={this.dateFormatter}>Fecha</TableHeaderColumn>
           <TableHeaderColumn dataField="seller" dataSort={true}>Vendedor</TableHeaderColumn>
           <TableHeaderColumn dataField="productId" dataSort={true}>ID Producto</TableHeaderColumn>
           <TableHeaderColumn dataField="product" dataSort={true}>Producto</TableHeaderColumn>
